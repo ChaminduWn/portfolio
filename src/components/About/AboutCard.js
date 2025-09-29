5297acf9dc6e29477b5742897660ea68cfff316b
ChaminduWN <127916715+chamindu2001@users.noreply.github.com>
2025-09-29 13:29:25 +0530

    add my cv and modify aboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,13 +8,13 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="purple"> Chamindu W.N </span>
+            from <span className="purple"> Homagama, Sri Lanka.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
+            I am currently Studied at SLIIT.
             <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
+            I have following BSc(Hons) in Information Technology at 
+            SLIIT
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
@@ -24,7 +24,7 @@ function AboutCard() {
               <ImPointRight /> Playing Games
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
+              <ImPointRight /> Social Services
             </li>
             <li className="about-activity">
               <ImPointRight /> Travelling
